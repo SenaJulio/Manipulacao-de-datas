610f0869371ff9f8144718b14734116528a23f94
JULIO SENA <gm.sena@hotmail.com>
2024-09-13 20:47:56 -0300

    continuação

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-let tempo = new Date();
-let time = new Date()
-console.log(tempo)
+let time = new Date();
+
+time.setFullYear(2020)
+time.setDate(2)
+
+time.setDate(time.getDate() + 243);
+time.setDate(time.getMonth() + 1);
+time.setHours(time.getHours() + 12)
+
+let year = time.getFullYear();
+let month = time.getMonth();
+let day = time.getDate();
+let weekday = time.getDay();
+
+let hours = time.getHours();
+let minutes = time.getMinutes();
+let seconds = time.getSeconds();
+let mili = time.getMilliseconds();
+
+if(minutes <10){
+    minutes ='0' + minutes
+}
+
+let hora = hours+ ":"+minutes
+
+
+console.log(year, month, day)
+console.log(hours, minutes, seconds)
